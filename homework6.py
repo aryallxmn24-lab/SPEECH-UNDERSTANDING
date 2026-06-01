b56f903986d63ec77fa5732a9b617bb6393d2f2f
Aryal Laxman <aryal.lxmn24@gmail.com>
2026-06-01 23:01:25 +0900

    homework6 committed

---
+++ homework6.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def minimum_Fs(f):
+    
+    Fs = 0  # change this line
+    return Fs
+
+def omega(f, Fs):
+    
+    omega = 0  # change this line
+    return omega
+
+def pure_tone(omega, N):
+   
+    x = 0 # change this line
+    return x
